970b1fcec5602b912ce2b1460a8e0abd778f1bc5
Glenn Dumoulin <glenn.dumoulin2001@gmail.com>
2023-02-14 16:24:36 +0100

    Added Click Registering
    Added Clickable Component;
    Fixed Some Naming Conventions;

--- MotherFroggers/Assets/Scripts/Tiles/WorldTile.cs
+++ MotherFroggers/Assets/Scripts/Tiles/WorldTile.cs
@@ -4,10 +4,4 @@ using UnityEngine;
 
 public class WorldTile : BaseTile
 {
-    private GameObject _tower = null;
-    public GameObject Tower
-    {
-        get { return _tower; }
-        set { _tower = value; }
-    }
 }
